df700ab036be90da2cfcc29a2ff5d066245628cc
sr1045 <sr1045@outlook.com>
2023-04-18 07:18:48 +0530

    Added getters and Setters

---
+++ src/main/java/models/Tempo.java
@@ -0,0 +1,20 @@
+package main.java.models;
+
+public class Tempo {
+	private String name;
+	private int age; 
+	
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main method");
+	}
+
+}
